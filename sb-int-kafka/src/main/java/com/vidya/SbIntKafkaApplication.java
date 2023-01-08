26a131064a9aadd786416ef084ce14e83450fb60
Vidya Sagar Gupta <v3sagar@gmail.com>
2023-01-08 20:58:49 +0530

    sb-int-kafka: project added

---
+++ sb-int-kafka/src/main/java/com/vidya/SbIntKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.vidya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbIntKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbIntKafkaApplication.class, args);
+	}
+
+}
---
+++ sb-int-kafka/src/test/java/com/vidya/SbIntKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbIntKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
